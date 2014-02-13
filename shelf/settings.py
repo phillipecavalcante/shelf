d6b62a2967702368636ac6b6ba3a85ce6297666f
Phillipe <cavalcante.phillipe@gmail.com>
2014-02-13 12:38:37 -0200

    Auto change static variable to Win or Mac or Linux path

--- shelf/settings.py
+++ shelf/settings.py
@@ -55,8 +55,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/Users/phillipe/Downloads/media/'
-# MEDIA_ROOT = 'C:/Users/phillipe/Downloads/media/'
+MEDIA_ROOT = 'C:/Users/phillipe/Downloads/media/' if os.name is 'nt' else '/Users/phillipe/Downloads/media/' 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -67,8 +66,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/Users/phillipe/Downloads/static/'
-# STATIC_ROOT = 'C:/Users/phillipe/Downloads/static/'
+STATIC_ROOT = 'C:/Users/phillipe/Downloads/static/' if os.name is 'nt' else '/Users/phillipe/Downloads/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
