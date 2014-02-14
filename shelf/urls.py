ca2deb71667dfb6188930eeebc953ab61d608ee9
Phillipe <cavalcante.phillipe@gmail.com>
2014-02-14 16:47:33 -0200

    Aperfeiçoando Gallery

--- shelf/urls.py
+++ shelf/urls.py
@@ -4,6 +4,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+
 # from django.contrib import admin
 # admin.autodiscover()
 
